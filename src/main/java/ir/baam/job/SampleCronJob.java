dc3f5d6ec13b845cd0eb841b9fdae6ac4a798ac5
ha.moradi <hamedmoradi.mailsbox@gmail.com>
2022-04-03 21:23:15 +0430

    send a request to standing-order service with rest template

--- src/main/java/ir/baam/job/SampleCronJob.java
+++ src/main/java/ir/baam/job/SampleCronJob.java
@@ -10,9 +10,17 @@ import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Slf4j
 @DisallowConcurrentExecution
@@ -20,14 +28,18 @@ public class SampleCronJob extends QuartzJobBean {
     @Autowired
     SchedulerRepository schedulerRepository;
 
+    RestTemplate restTemplate=new RestTemplate();
+
+
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
         String triggerName = context.getTrigger().getJobKey().getName();
         SchedulerJobInfo schedulerJobInfo = schedulerRepository.findByJobName(triggerName);
+        log.info("SampleCronJob Start................");
         if (schedulerJobInfo.getServiceType().equals("RECURRING")) {
-            log.info("salaaaaaaaaaaaaaaam");
+            log.info("send a request to standing-order service");
+       apiCall("hello","http://localhost:9027/standing-order/execute/");
         } else {
-            log.info("SampleCronJob Start................");
             IntStream.range(0, 10).forEach(i -> {
                 log.info("Counting - {}", i);
                 try {
@@ -39,4 +51,13 @@ public class SampleCronJob extends QuartzJobBean {
         }
         log.info("SampleCronJob End................");
     }
+
+    private void apiCall(String message, String url) {
+        HttpHeaders headers = new HttpHeaders();
+//        headers.setBearerAuth(token);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> request = new HttpEntity<String>(message, headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+    }
+
 }
