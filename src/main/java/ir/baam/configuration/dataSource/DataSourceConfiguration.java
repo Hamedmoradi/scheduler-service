3e77be3531e8e2cec6a306281285440fea2a3e43
ha.moradi <hamedmoradi.mailsbox@gmail.com>
2022-06-12 19:06:24 +0430

    BAMD-26667:change application name and name space and production configuration
    Module [scheduler-service]:
      * DataSourceConfiguration.java
      * application-dev.yml
      * application.yml
      * Dockerfile
      * docker-compose.yml
      * kuber-deployment.yml
      * kuber-service.yml
      * pom.xml

--- src/main/java/ir/baam/configuration/dataSource/DataSourceConfiguration.java
+++ src/main/java/ir/baam/configuration/dataSource/DataSourceConfiguration.java
@@ -1,25 +1,31 @@
 package ir.baam.configuration.dataSource;
 
-
+import lombok.RequiredArgsConstructor;
 import oracle.jdbc.datasource.impl.OracleDataSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
 @Configuration
+@RequiredArgsConstructor
 public class DataSourceConfiguration {
-
+    @Value("${spring.scheduler-datasource.username}")
+    private  String user;
+    @Value("${spring.scheduler-datasource.password}")
+    private  String password;
+    @Value("${spring.scheduler-datasource.jdbcUrl}")
+    private  String url;
     @Bean
     public DataSource getDataSource() {
 
         OracleDataSource dataSource = null;
         try {
             dataSource = new OracleDataSource();
-            dataSource.setUser("MT");
-            dataSource.setPassword("kjio0988");
-            dataSource.setURL("jdbc:oracle:thin:@//172.30.41.22:1521/offpayapdb1");
+            dataSource.setUser(user);
+            dataSource.setPassword(password);
+            dataSource.setURL(url);
             dataSource.setImplicitCachingEnabled(true);
             return dataSource;
         } catch (SQLException e) {
@@ -27,7 +33,5 @@ public class DataSourceConfiguration {
         }
         return null;
     }
-
-
-    //todo config hikari
 }
+
