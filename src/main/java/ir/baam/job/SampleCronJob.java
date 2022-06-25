769d4e106fe689308112b136eda8028b2f0fa716
ha.moradi <hamedmoradi.mailsbox@gmail.com>
2022-06-25 20:11:19 +0430

    BAMD-26667:change application version
    Module [scheduler-service]:
      * SchedulerCommandDto.java
      * SampleCronJob.java
      * pom.xml

--- src/main/java/ir/baam/job/SampleCronJob.java
+++ src/main/java/ir/baam/job/SampleCronJob.java
@@ -49,7 +49,7 @@ public class SampleCronJob extends QuartzJobBean {
         log.info("SampleCronJob Start................");
         if (schedulerJobInfo.getServiceType().equals(RECURRING) && schedulerJobInfo.getCommand().equals(CommandEnumeration.INITIATE.getValue())) {
             log.info("send a instruction request to standing-order service");
-            SchedulerCommandDto schedulerCommand = new SchedulerCommandDto(new Date().getTime(), CommandEnumeration.INITIATE.getValue(), null, StandingOrderTransactionStatusEnum.PENDING.value());
+            SchedulerCommandDto schedulerCommand = new SchedulerCommandDto(new Date(), CommandEnumeration.INITIATE.getValue(), null, StandingOrderTransactionStatusEnum.PENDING.value());
             ResponseEntity initiateResult = standingOrderClient.initiateCommand(schedulerClientTokenManager.getClientToken(), schedulerCommand);
             if (initiateResult.getStatusCodeValue() == 200) {
                 //reschedule for next time
@@ -64,7 +64,7 @@ public class SampleCronJob extends QuartzJobBean {
         if (schedulerJobInfo.getServiceType().equals(RECURRING) && schedulerJobInfo.getCommand().equals(CommandEnumeration.EXECUTE.getValue())) {
             log.info("send a execute request to standing-order service");
 
-            SchedulerCommandDto schedulerCommand = new SchedulerCommandDto(new Date().getTime(), CommandEnumeration.EXECUTE.getValue(), null, StandingOrderTransactionStatusEnum.PROCESSING.value());
+            SchedulerCommandDto schedulerCommand = new SchedulerCommandDto(new Date(), CommandEnumeration.EXECUTE.getValue(), null, StandingOrderTransactionStatusEnum.PROCESSING.value());
             ResponseEntity result = standingOrderClient.executeCommand(schedulerClientTokenManager.getClientToken(), schedulerCommand);
             if (result.getStatusCodeValue() == 200) {
                 //reschedule for next time
@@ -79,7 +79,7 @@ public class SampleCronJob extends QuartzJobBean {
         if (schedulerJobInfo.getServiceType().equals(RECURRING) && schedulerJobInfo.getCommand().equals(CommandEnumeration.RESCHEDULE_FOR_FAILED_INSTRUCTIONS.getValue())) {
             log.info("send a instruction request for initiation_failed to standing-order service");
 
-            SchedulerCommandDto schedulerCommand = new SchedulerCommandDto(new Date().getTime(), CommandEnumeration.INITIATE.getValue(), null, StandingOrderTransactionStatusEnum.INITIATION_FAILED.value());
+            SchedulerCommandDto schedulerCommand = new SchedulerCommandDto(new Date(), CommandEnumeration.INITIATE.getValue(), null, StandingOrderTransactionStatusEnum.INITIATION_FAILED.value());
             ResponseEntity initiationFailedResult = standingOrderClient.initiateCommand(schedulerClientTokenManager.getClientToken(), schedulerCommand);
             if (initiationFailedResult.getStatusCodeValue() == 200) {
                 //reschedule for next time
@@ -94,7 +94,7 @@ public class SampleCronJob extends QuartzJobBean {
         if (schedulerJobInfo.getServiceType().equals(RECURRING) && schedulerJobInfo.getCommand().equals(CommandEnumeration.RESCHEDULE_FOR_FAILED_TRANSACTIONS.getValue())) {
             log.info("send a execution request for execution_failed to standing-order service");
 
-            SchedulerCommandDto schedulerCommand = new SchedulerCommandDto(new Date().getTime(), CommandEnumeration.EXECUTE.getValue(), null, StandingOrderTransactionStatusEnum.EXECUTION_FAILED.value());
+            SchedulerCommandDto schedulerCommand = new SchedulerCommandDto(new Date(), CommandEnumeration.EXECUTE.getValue(), null, StandingOrderTransactionStatusEnum.EXECUTION_FAILED.value());
             ResponseEntity failedExecution = standingOrderClient.executeCommand(schedulerClientTokenManager.getClientToken(), schedulerCommand);
             if (failedExecution.getStatusCodeValue() == 200) {
                 //reschedule for next time
--- src/main/java/ir/baam/webClient/standingOrder/dto/SchedulerCommandDto.java
+++ src/main/java/ir/baam/webClient/standingOrder/dto/SchedulerCommandDto.java
@@ -3,12 +3,18 @@ package ir.baam.webClient.standingOrder.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Date;
 
 
-@AllArgsConstructor
 @Data
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class SchedulerCommandDto {
-    private Long date;
+
+
+    private Date date;
     private String command;
     private Long count;
     private String transactionStatus;
